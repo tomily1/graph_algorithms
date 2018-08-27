6d3b3a5fec13492551e9b73b7884216136a1976c
Israel Tomilayo <israel.tomilayo@andela.com>
2018-08-27 10:32:00 +0100

    create depth first search

--- lib/depth_first_search.rb
+++ lib/depth_first_search.rb
@@ -1,10 +1,38 @@
 class DepthFirstSearch
+  attr_reader :tree, :value, :result
+
   def initialize(tree)
+    @tree = tree
   end
 
   def traverse
+    @value = nil
+    empty_result
+    traverse_or_search(@tree)
+    result
   end
 
   def search(value)
+    @value = value
+    empty_result
+    traverse_or_search(@tree)
+    result
+  end
+
+  private
+
+  def empty_result
+    @result = []
+  end
+
+  def traverse_or_search(node)
+    if @value.nil?
+      result << node.value
+    else
+      result << node.value if @value == node.value
+    end
+    node.children.each do |child|
+      traverse_or_search(child)
+    end
   end
 end
